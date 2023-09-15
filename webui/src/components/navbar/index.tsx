d970b7e908ce88baf2b274c233624d4b8fc56e47
Darby Ross <52804447+rossd84@users.noreply.github.com>
2023-09-14 19:56:40 -0500

    add navbar

---
+++ webui/src/components/navbar/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className='flex w-full h-[80px] justify-center items-center'>
+      <ul className="flex gap-8">
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/cart'>Cart</Link></li>
+        <li><Link to='/admin'>Admin</Link></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
